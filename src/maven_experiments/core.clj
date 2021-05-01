7ffaa283ce91935a192991082af9727f3a135933
Pascal Ellenberger <pascal.ellenberger@protonmail.com>
2021-05-01 17:34:43 +0200

    add a way to run mvn on a given project and parse result

--- src/maven_experiments/core.clj
+++ src/maven_experiments/core.clj
@@ -1,7 +1,22 @@
 (ns maven-experiments.core
+  (:require [clojure.java.shell :as shell]
+            [clojure.set]
+            [clojure.string :as str])
   (:gen-class))
 
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  "Run a timing experiment for maven"
+  [path]
+  (defn enrich [output]
+    (let [x (-> output
+                (:err)
+                (str/split #"\n"))
+          y (->>  x ; could have used as-> to change parameter position. Maybe there is a better way with the transducer?
+                  (drop 1) 
+                  (vec)
+                  (map #(str/split % #"\t"))
+                  (into output))] 
+      y)) ; todo: make all keys keywords
+  (-> (shell/sh "/bin/sh" "-c" (str "cd " path " && time mvn clean install > /dev/null")) ; todo: pass -T argument
+      (enrich)
+      (clojure.set/rename-keys {:err :res "real" :real "user" :user "sys" :sys}))) ; for some reason time writes to err
