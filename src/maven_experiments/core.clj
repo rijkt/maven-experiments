b191c3723e034f1001c3bd617def37c1d5347584
Pascal Ellenberger <pascal.ellenberger@protonmail.com>
2021-05-01 18:05:10 +0200

    improve enrich fn

--- src/maven_experiments/core.clj
+++ src/maven_experiments/core.clj
@@ -8,15 +8,17 @@
   "Run a timing experiment for maven"
   [path]
   (defn enrich [output]
-    (let [x (-> output
-                (:err)
-                (str/split #"\n"))
-          y (->>  x ; could have used as-> to change parameter position. Maybe there is a better way with the transducer?
-                  (drop 1) 
-                  (vec)
-                  (map #(str/split % #"\t"))
-                  (into output))] 
-      y)) ; todo: make all keys keywords
+    (let [time-result (-> output
+                          (:err) ; for some reason time writes to err
+                          (str/split #"\n"))
+          enriched (->>  time-result ; could have used as-> to change parameter position. Maybe there is a better way with the transducer?
+                         (drop 1) ; there is a newline at the beginning of the output
+                         (vec)
+                         (map #(str/split % #"\t"))
+                         (into output))
+          trimmed (-> enriched
+                      (clojure.set/rename-keys {:err :res "real" :real "user" :user "sys" :sys})
+                      (dissoc :out))] 
+      trimmed))
   (-> (shell/sh "/bin/sh" "-c" (str "cd " path " && time mvn clean install > /dev/null")) ; todo: pass -T argument
-      (enrich)
-      (clojure.set/rename-keys {:err :res "real" :real "user" :user "sys" :sys}))) ; for some reason time writes to err
+      (enrich)))
