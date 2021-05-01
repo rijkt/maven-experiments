0719158cfe34e3fd69e61fb0070fc58e273502ce
Pascal Ellenberger <pascal.ellenberger@protonmail.com>
2021-05-01 13:58:53 +0200

    inital commit

---
+++ test/maven_experiments/core_test.clj
@@ -0,0 +1,7 @@
+(ns maven-experiments.core-test
+  (:require [clojure.test :refer :all]
+            [maven-experiments.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
